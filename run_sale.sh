04c622405049b9a9458cd61b5f34deffd1a6020a
Derek Chen <35285430+DerekYC@users.noreply.github.com>
2022-02-23 21:00:34 -0500

    Add files via upload

---
+++ run_sale.sh
@@ -0,0 +1,107 @@
+
+python download_sale.py acura ILX TSX TLX RLX NSX RDX MDX ZDX CL TL RL RSX Vigor Legend SLX
+
+python download_sale.py alfa-romeo Giulia Stelvio 4C Spider 164 Milano
+# skip Eagle AMC Aston Martin
+# delete 
+# 'a8' 'a8 / s8' '80 90' '80 / 90'  'allroad' 'allroad quattro'  'allroad' 'allroad quattro' 
+# no e-Tron 
+python download_sale.py audi A3 A4 A5 A6 A7 A8 TT R8 A4 80-90 Allroad Q3 Q5 Q7 Q8 Allroad Quattro Cabriolet 4000 Coupe Quattro Fox 100 5000 V8
+# skip bentley 
+# no x8
+python download_sale.py bmw 1-series 2-series 3-series 4-series 5-series 6-series 7-series 8-series i3 i8 Z4 X1 X2 X3 X4 X5 X6 X7 Z3
+
+python download_sale.py buick Verano Cascada Regal LaCrosse Encore Envision Enclave Century LeSabre Park-Avenue Lucerne Reatta Terraza Rendezvous Rainier
+# 'seville sls' 'seville'  'fleetwood' 'fleetwood (brougham)'
+python download_sale.py cadillac ATS CT5 CT6 XTS ELR XT4 XT5 XT6 Escalade Escalade-ESV Escalade-EXT CTS STS DTS Seville-SLS DeVille SRX XLR Cimarron Catera Eldorado Allante Fleetwood
+# no ss(*14-17)  geo-storm:us-geo-storm skip
+python download_sale.py chevrolet Spark Sonic Cruze Bolt-EV Volt Malibu Impala Caprice SS Camaro Corvette Trax Equinox Blazer Traverse Tahoe Suburban Colorado Silverado City-Express Aveo Cavalier Cobalt Monte-Carlo HHR SSR Captiva-Sport Avalanche El-Camino S10 Trailblazer Tracker Venture Uplander Astro Metro Spectrum Prizm Lumina
+
+python download_sale.py chrysler 200 300 Pacifica-minivan Town-Country PT-Cruiser Sebring Cirrus Concorde LHS E-Class Imperial Fifth-Avenue New-Yorker Crossfire Conquest Laser Cordoba Aspen Pacifica Prowler Voyager TC-Maserati
+
+python download_sale.py daewoo Lanos Nubira Leganza
+# skip daihatsu 
+# 'omni 024 charger' 'omni 024 / charger'
+# delete Ram-pick-up 
+python download_sale.py dodge Dart Challenger Charger Viper Grand-Caravan Journey Durango Caliber Neon Shadow Omni Omni-024-Charger Colt Avenger Stratus Spirit Aries 600 400 Magnum Intrepid Monaco Dynasty Diplomat Nitro Avenger-Coupe Stealth Colt-Challenger Conquest Daytona Lancer Colt-Vista Raider Rampage
+python download_sale.py ram dakota sprinter cargo-van
+# skip ferrari
+python download_sale.py fiat 500 500L 500X 124-Spider
+# delete c-max Police-Interceptor-sedan
+# 'five hundred' 'five hundred / taurus'  'freestyle taurus x' 'freestyle / taurus x'
+python download_sale.py ford Fiesta Focus Fusion Five-Hundred Taurus Mustang EcoSport Escape Edge Flex Explorer Expedition Transit-Connect Transit E-series Ranger F-series GT police-interceptor-suv Escort Taurus Crown-Victoria Freestyle-Taurus-X Windstar Freestar Excursion Thunderbird Probe
+
+python download_sale.py genesis G70 G80 G90
+
+python download_sale.py gmc Terrain Acadia Yukon Yukon-XL Canyon Sierra Savana Sonoma Jimmy Envoy Safari
+# delete clarity-fcv/
+python download_sale.py honda  Fit Civic Accord Odyssey HR-V CR-V Passport Pilot Ridgeline CR-Z Insight Crosstour FCX Element CRX Del-Sol S2000
+# skip hammer
+# 'azera' 'azera / xg350'
+python download_sale.py hyundai Accent Elantra Ioniq Sonata Azera Veloster Venue Kona Tucson Nexo Santa-Fe Palisade Veracruz Tiburon Entourage Excel Scoupe Genesis-sedan Genesis-Coupe Equus 
+# 'q70' 'q70 / m' 'qx50' 'qx50 / ex' ...
+python download_sale.py infiniti Q50 Q60 Q70 QX50 QX60 QX70 QX80 G Q40 I35 J30 Q45 QX4 G20 
+# 'i series pickup' 'pickup i-280 / i-350' 'i mark' 'i-mark'
+python download_sale.py isuzu VehiCROSS Rodeo-Sport Rodeo Axiom Trooper Ascender i-series-pickup Stylus Impulse I-Mark 
+
+python download_sale.py jaguar XE XF XJ F-type E-Pace F-Pace I-Pace XK X-Type S-Type XJS
+
+python download_sale.py jeep Renegade Patriot Wrangler Gladiator Cherokee Grand-Cherokee Compass Liberty Commander Cherokee-XJ Wagoneer-XJ Grand-Wagoneer Comanche Scrambler
+
+python download_sale.py kia Rio Soul Forte Optima Stinger Cadenza K900 Sedona Niro Seltos Sportage Sorento Telluride Spectra Amanti Rondo Borrego
+# skip lamborghini Lancia
+python download_sale.py land-rover Discovery-Sport LR4-LR3-Discovery LR2-Freelander Defender Range-Rover-Evoque* Range-Rover-Velar* Range-Rover-Sport* Range-Rover-Classic*
+
+python download_sale.py lexus CT IS RC ES GS LC LS UX NX RX GX LX HS SC LFA
+# delete Pre-2002 Continental
+# 'mkz', 'mkz / zephyr'  'continental new', 'continental 2017-'  'corsair mkc', 'mkc'
+python download_sale.py lincoln MKZ continental-new MKT Corsair-MKC MKX Aviator Navigator MKS LS Town-Car Mark-LT Blackwood
+# delete rx-8
+# 'mx 5', 'mx-5 miata' 'protege', 'protégé'
+python download_sale.py mazda Mazda2* Mazda3* Mazda6* Mazda5* MX-5-Miata CX-3 CX-30 CX-5 CX-9 Protege RX-7 MPV Tribute CX-7 B-Series
+# skip mclaren 
+# 'sls amg', 'sls'
+python download_sale.py mercedes-benz A-Class B-Class CLA C-Class E-Class CLS S-Class SLK SL AMG-GT GLA GLB GLC GLE G-Class GL Metris Sprinter CLK CL SLS-AMG SLR-McLaren GLK R-Class M-Class 190
+# delete scorpio xr4ti
+python download_sale.py mercury Sable Milan Montego-Sable Grand-Marquis Cougar Villager Monterey Mariner Mountaineer Merkur-Scorpio XR4Ti
+# skip mg
+# 'cooper' 'hardtop 2-door' 'hardtop 4 door' 'hardtop 4-door'
+python download_sale.py mini Cooper hardtop-4-door Convertible Clubman Coupe Roadster Countryman Paceman
+# stop at eclipse-cross
+# delete Eclipse-Cross
+python download_sale.py mitsubishi i-MiEV Mirage Lancer Outlander-Sport Outlander Outlander-PHEV Galant Diamante Eclipse 3000GT Starion Cordia Tredia Expo-LRV Expo Endeavor Montero Montero-Sport Mighty-Max Raider Wagon Precis
+# 'x terra', 'xterra'
+python download_sale.py nissan Versa Sentra Altima Maxima Leaf 370Z GT-R Kicks Rogue Pathfinder Murano Armada Frontier Titan NV200 NV Cube Quest Juke X-Terra Wagon 350Z 300ZX
+# skip oldsmobile peugeot 
+# 'horizon tc3 turismo', 'horizon tc3/turismo' 'colt vista', 'vista' 'colt champ', 'colt/champ'
+python download_sale.py plymouth Voyager colt-vista Neon Sundance Breeze Acclaim Reliant Caravelle Gran-Fury Horizon horizon-tc3-turismo Colt-Champ Laser Conquest Arrow Sapporo Prowler Scamp
+# 'g3', 'g3 wave'   'grand', 'grand am'
+python download_sale.py pontiac G3 Vibe Sunfire G5 Grand-Am G6 Grand-Prix Bonneville G8 GTO Firebird Solstice Montana Aztek Torrent LeMans
+
+python download_sale.py porsche Boxster Cayman 718-boxster-cayman 911 Taycan-EV Panamera Macan Cayenne 918-Spyder Carrera-GT 924S 944 968 928
+# skip rolls-royce 
+# '92x', '9-2x' ...
+python download_sale.py saab 92X 93 95 94X 97x 900 900-Convertible 9000
+# 'general motors ev1', 'ev1'
+python download_sale.py saturn S-Series Ion Astra L-series Aura Sky Vue Relay Outlook General-Motors-EV1*
+
+python download_sale.py scion iQ xA xB xD iA iM tC FR-S
+# skip smart sterling
+# 'wrx sti', 'wrx / sti'  'leone', 'leone / loyale'
+python download_sale.py subaru Impreza WRX-STi XV-Crosstrek Legacy Outback Forester BRZ Ascent SVX XT Tribeca Baja Brat Leone FF-1 360
+# 'reno forenza', 'forenza / reno'  'xl7', 'xl-7'
+python download_sale.py suzuki Aerio SX4 Reno-Forenza Verona Kizashi Vitara Grand-Vitara Vitara XL7 Equator
+
+python download_sale.py tesla Model-S Model-X Model-3
+# 'highlander', 'highlander ' 'corolla hatchback', 'corolla hatchback / im' 'prius', 'prius  family'  'pickup', 'pickup (hilux)'   'mr2 spyder', 'mr2'
+# delete 86 
+python download_sale.py toyota Yaris corolla-sedan corolla-hatchback Prius Camry Venza Avalon Sienna C-HR RAV4 4Runner Highlander FJ-Cruiser Sequoia Land-Cruiser Tacoma Tundra Mirai Tercel Echo Cressida Van Previa Paseo Celica MR2-Spyder Supra T100 Pickup
+# skip triumph
+# 'tiguan l', 'tiguan l (2018-)'
+python download_sale.py volkswagen Golf Beetle Jetta Passat Arteon tiguan-l Atlas Touareg CC Eos Tiguan Routan Phaeton Quantum Fox Scirocco Corrado Cabriolet Eurovan Vanagon Rabbit-Pickup Karmann-Ghia
+# 's60 v60', 's60 / v60' 's90 v90', 's90/v90'
+# delete v90-cross-country
+python download_sale.py volvo S60-V60 S90-V90 XC40 XC60 XC70 XC90 S60-Cross-Country V60-Cross-Country S90 V90 C70 V70 S80 C30 S40 V50
+# skip yugo
+python aggregate_sale.py
+
